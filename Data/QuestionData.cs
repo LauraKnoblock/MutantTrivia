516dc1bf746f58bb280451b337c25fac7a7dc5ae
Your Name <laura.e.knoblock@gmail.com>
2024-09-27 12:29:32 -0500

    Models

---
+++ Data/QuestionData.cs
@@ -0,0 +1,34 @@
+﻿using MutantTrivia.Models;
+
+namespace MutantTrivia.Data
+{
+    public class QuestionData
+    {
+        static private Dictionary<int, Question> 
+            Questions = new Dictionary<int, Question>();
+
+        public static IEnumerable<Question> GetAll()
+        {
+            return Questions.Values;
+        }
+
+        // Add a new question to my Dictionary
+
+        public static void Add(Question newQuestion)
+        {
+            Questions.Add(newQuestion.Id, newQuestion);
+        }
+
+        public static void Remove(int id)
+        {
+            Questions.Remove(id);
+        }
+
+        // Fetch specific event
+
+        public static Question GetById(int id)
+        {
+            return Questions[id];
+        }
+    }
+}
---
+++ Models/Question.cs
@@ -0,0 +1,40 @@
+﻿namespace MutantTrivia.Models
+{
+    public class Question
+    {
+        public string Name { get; set; }
+        public string Answer { get; set; }
+
+        public int Id { get; set; }
+        static private int nextId = 1;
+
+        public Question()
+        {
+            Id = nextId;
+            nextId++;
+        }
+
+        public Question(string name, string answer)
+        {
+            Name = name;
+            Answer = answer;
+            Id = nextId;
+            nextId++;
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is Question @question && Id == @question.Id;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Id);
+        }
+    }
+}
